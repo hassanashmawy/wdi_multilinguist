e2cd1dbcba0f9e4d5702c8bcd10730700eadc2c0
hassanashmawy <hassan.ashmawy@gmail.com>
2019-05-20 20:08:01 -0400

    done

--- multilinguist.py
+++ multilinguist.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import random
+import math
 
 class Multilinguist:
   """This class represents a world traveller who knows 
@@ -26,12 +28,10 @@ class Multilinguist:
   def language_in(self, country_name):
     """Uses the RestCountries API to look up one of the languages
     spoken in a given country
-
     Parameters
     ----------
     country_name : str
          The full name of a country.
-
     Returns
     -------
     bool 
@@ -45,12 +45,10 @@ class Multilinguist:
   def travel_to(self, country_name):
     """Sets current_lang to one of the languages spoken
     in a given country
-
     Parameters
     ----------
     country_name : str
         The full name of a country.
-
     Returns
     -------
     str
@@ -62,12 +60,10 @@ class Multilinguist:
 
   def say_in_local_language(self, msg):
     """(Roughly) translates msg into current_lang using the Transltr API
-
     Parameters
     ----------
     msg : str
         A message to be translated.
-
     Returns
     -------
     str
@@ -78,3 +74,91 @@ class Multilinguist:
     json_response = json.loads(response.text)
     return json_response['translationText']
 
+
+class MathGenius(Multilinguist):
+
+  def report_total(self, numbers):
+    return 'The total is ' + str(sum(numbers))
+
+  def square_root(self, x):
+    return 'The square root is ' + str(math.sqrt(x))
+
+class QuoteCollector(Multilinguist):
+
+  def __init__(self):
+    self.quote_collection = []
+
+  def add_quote(self, quote):
+    self.quote_collection.append(quote)
+
+  def random_quote(self):
+    return self.quote_collection[random.randint(0,(len(self.quote_collection) - 1))]
+
+class FoodRecommender(Multilinguist):
+
+  def __init__(self):
+    self.foodbycontinent = {
+      'North America': "Willie Mae's Scotch House in New Orleans, Louisiana",
+      'South America': 'Peumayen Resturant in Santiago, Chile',
+      'Africa': 'Cape Malay food in Cape Town',
+      'Europe': 'Figlmüller in Vienna',
+      'Asia': 'In Love Restaurant in Bangkok',
+      'Australia': 'Broad Arrow in the Western Australia Outback',
+      'Antarctica': 'You should bring your own granola bars'
+    }
+
+  def ask(self):
+    print("Please enter a continent to get a food recommendation.")
+    user_input = input()
+    for continent, recommendation in self.foodbycontinent.items():
+      if continent == user_input:
+        return "You should go to: " + recommendation
+
+
+her = Multilinguist()
+print(her)
+
+print(her.language_in('Egypt'))
+print(her.travel_to('Iran'))
+print(her.say_in_local_language('Hello, Piss off'))
+
+me = MathGenius()
+print(me.report_total([23,45,676,34,5778,4,23,5465])) # The total is 12048
+print(me.square_root(4))
+me.travel_to("India")
+print(me.say_in_local_language(me.report_total([6,3,6,68,455,4,467,57,4,534]))) # है को कुल 1604
+print(me.say_in_local_language(me.square_root(4)))
+me.travel_to("Italy")
+print(me.say_in_local_language(me.report_total([324,245,6,343647,686545]))) # È Il totale 1030767
+print(me.say_in_local_language(me.square_root(4)))
+
+you = QuoteCollector()
+you.add_quote('Now! I will listen now!')
+you.add_quote('show me a trick')
+you.add_quote('Pizza baby!!')
+print(you.quote_collection)
+print(you.random_quote())
+you.travel_to('Italy')
+print(you.say_in_local_language(you.random_quote()))
+
+we = FoodRecommender()
+print(we.ask())
+we.travel_to('Italy')
+print(we.say_in_local_language(we.ask()))
+
+"""
+# him = MathGuy()
+# print(him.do_the_math([23,45,676,34,5778,4,23,5465])) # The total is 12048
+# him.travel_to("India")
+# print(him.do_the_math([6,3,6,68,455,4,467,57,4,534])) # है को कुल 1604
+# him.travel_to("Italy")
+# print(him.do_the_math([324,245,6,343647,686545])) # È Il totale 1030767
+
+
+
+speak_2_lang = Multilinguist()
+speak_2_lang.travel_to('Egypt')
+# speak_2_lang.say_in_local_language('you are fuckin cool')
+# print(speak_2_lang.say_in_local_language('you are fuckin cool'))
+
+"""
